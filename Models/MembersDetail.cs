f56356f4c14246993f89f884d8d7a59cf99201c5
Renwu <renwu072@icloud.com>
2020-12-14 20:09:08 +0800

    加入專案檔案。

---
+++ Models/MembersDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemberProfile.Models
+{
+    public class MembersDetail
+    {
+        [Key]
+        public int MemberDetailId { get; set; }
+
+        [Column (TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime Birthday { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Gender { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+
+    }
+}
---
+++ Models/MembersDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemberProfile.Models
+{
+    public class MembersDetailContext:DbContext
+    {
+        public MembersDetailContext(DbContextOptions<MembersDetailContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<MembersDetail> MembersDetails { get; set; }
+    }
+}
