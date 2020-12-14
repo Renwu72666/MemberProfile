9a112738142f44eb9ad3a13d37d3023c92d2882a
Kevin <renwu072@icloud.com>
2020-12-14 22:35:55 +0800

    feat: add sql docker connection

---
+++ Migrations/20201214133629_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemberProfile.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MembersDetails",
+                columns: table => new
+                {
+                    MemberDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembersDetails", x => x.MemberDetailId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MembersDetails");
+        }
+    }
+}
